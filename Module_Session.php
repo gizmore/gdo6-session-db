fffc04819a6922e3edecc083e532fc5b9a77298c
gizmore <gizmore@wechall.net>
2021-05-19 07:01:43 +0200

    6.10.3-r1147_fixpack_and_joins

--- Module_Session.php
+++ Module_Session.php
@@ -5,6 +5,8 @@ use GDO\Core\GDO_Module;
 
 final class Module_Session extends GDO_Module
 {
+    public function isCoreModule() { return true; }
+    
     public function getClasses()
     {
         return [GDO_Session::class];
